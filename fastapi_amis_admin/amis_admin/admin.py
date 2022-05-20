340cae85262f01ccfc271ab296cafc068da4b5db
atomi <1456417373@qq.com>
2022-05-20 10:39:07 +0800

    fix: 修复`PageAdmin schemaApi`提交默认data数据

--- fastapi_amis_admin/amis_admin/admin.py
+++ fastapi_amis_admin/amis_admin/admin.py
@@ -640,7 +640,7 @@ class PageAdmin(PageSchemaAdmin, RouterAdmin):
         super().get_page_schema()
         if self.page_schema:
             self.page_schema.url = f'{self.router_path}{self.page_path}'
-            self.page_schema.schemaApi = AmisAPI(method='post', url=f'{self.router_path}{self.page_path}', cache=300000)
+            self.page_schema.schemaApi = AmisAPI(method='post', url=f'{self.router_path}{self.page_path}', data={}, cache=300000)
             if self.page_parser_mode == 'html':
                 self.page_schema.schema_ = Page(body=Iframe(src=self.page_schema.url))
         return self.page_schema
