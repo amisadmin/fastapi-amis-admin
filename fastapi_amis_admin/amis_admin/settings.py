06a03bc6bd2070f4933e4ca7418030e462b56adc
atomi <1456417373@qq.com>
2022-05-28 23:26:10 +0800

    perf: 添加项目默认`Settings`类字段

--- fastapi_amis_admin/amis_admin/settings.py
+++ fastapi_amis_admin/amis_admin/settings.py
@@ -3,6 +3,8 @@ from pydantic import BaseSettings, Field
 
 class Settings(BaseSettings):
     """项目配置"""
+    host: str = '127.0.0.1'
+    port: int = 8000
     debug: bool = False
     version: str = '0.0.0'
     site_url: str = ''
