585f8db207b10d2144d1a29e33057f85ee5c179d
atomi <1456417373@qq.com>
2022-06-07 22:59:07 +0800

    test: 更新amis组件模型附加字段测试

--- tests/test_amis/test_amis.py
+++ tests/test_amis/test_amis.py
@@ -9,7 +9,6 @@ def test_Page():
     assert page.amis_html().find(page_json)
 
 
-def test_PageSchema():
-    tmp = PageSchema(schema=Page(), children=[PageSchema()], tmp_field='tmp field')  # type: ignore
-    amis_json = '{"schema":{"type":"page"},"children":[{}],"tmp_field":"tmp field"}'
-    assert tmp.amis_json() == amis_json
+def test_extra_fields():
+    tmp = PageSchema(schema=Page(), children=[PageSchema()], extra_field='extra field')  # type: ignore
+    assert tmp.amis_dict().get('extra_field') == "extra field"
