9d4be8e6e0fa42b239b120d711a367a4ae6ca68b
atomi <1456417373@qq.com>
2022-06-16 10:58:58 +0800

    perf: upgrade Amis to 1.10.2

--- fastapi_amis_admin/admin/settings.py
+++ fastapi_amis_admin/admin/settings.py
@@ -14,7 +14,7 @@ class Settings(BaseSettings):
     database_url_async: str = Field(..., env='DATABASE_URL_ASYNC')
     language: str = ''  # 'zh_CN','en_US'
     amis_cdn: str = 'https://unpkg.com'
-    amis_pkg: str = 'amis@1.10.1'
+    amis_pkg: str = 'amis@1.10.2'
 
     @validator('amis_cdn', 'root_path', 'site_url', pre=True)
     def valid_url(url: str):
