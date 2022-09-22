69481d125d99fc04d9838fe3bd2db1744f762ee8
Chaoying <chaunceywe@gmail.com>
2022-09-22 09:48:35 +0800

    Fix test page json format

--- tests/test_amis/test_amis.py
+++ tests/test_amis/test_amis.py
@@ -2,7 +2,7 @@ from fastapi_amis_admin.amis.components import Page, PageSchema
 
 def test_Page():
     page = Page(title = '标题', body = 'Hello World!')
-    page_json = r'{"type":"page","title":"\u6807\u9898","body":"Hello World!"}'
+    page_json = r'{"type": "page", "title": "\u6807\u9898", "body": "Hello World!"}'
     assert page.amis_json() == page_json
     assert page.amis_dict() == {'type': 'page', 'title': '标题', 'body': 'Hello World!'}
     assert page.amis_html().find(page_json)
