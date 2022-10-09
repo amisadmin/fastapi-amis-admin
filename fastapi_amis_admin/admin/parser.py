0094c1a4f0a8218fbb8f501ca3a29fb9ad365cb2
swelcker <swelcker@expertsystem.com>
2022-10-09 18:07:36 +0200

    added de_DE language added i18n to pareser for item label and description added chache_clear to set_language() in i18n changed set_language() in i18n and only treat zh* languages with hardcoded language name 'zh_CN'

--- fastapi_amis_admin/admin/parser.py
+++ fastapi_amis_admin/admin/parser.py
@@ -108,11 +108,13 @@ class AmisParser:
                 item.value = modelfield.default
         item.name = modelfield.alias
         if item.label is None:
-            item.label = modelfield.field_info.title or modelfield.name
+            item.label = _(modelfield.field_info.title) or _(modelfield.name)  # The use of I18N
+        else:
+            item.label = _(modelfield.field_info.title)  # The use of I18N
 
         label_name = "labelRemark" if isinstance(item, FormItem) else "remark"
         if getattr(item, label_name, None) is None:
-            label = Remark(content=modelfield.field_info.description) if modelfield.field_info.description else None
+            label = Remark(content=_(modelfield.field_info.description)) if modelfield.field_info.description else None  # The use of I18N
             setattr(item, label_name, label)
         return item
 
