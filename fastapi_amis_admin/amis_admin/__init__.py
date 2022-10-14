d420320960035647c2954b86df4ccc3c399bea43
atomi <1456417373@qq.com>
2022-10-14 23:05:54 +0800

    perf: Remove the `fastapi amis admin.amis admin` package

--- fastapi_amis_admin/amis_admin/__init__.py
+++
@@ -1,6 +0,0 @@
-"""This package is for compatibility with v0.1.8 and below,v0.3.0 and above will be removed. """
-import logging
-
-logging.warning(
-    "The `fastapi_amis_admin.amis_admin` package is deprecated, " "please migrate to the `fastapi_amis_admin.admin` package"
-)
--- fastapi_amis_admin/amis_admin/parser.py
+++
@@ -1,2 +0,0 @@
-"""This package is for compatibility with v0.1.8 and below,v0.3.0 and above will be removed. """
-from fastapi_amis_admin.admin.parser import AmisParser
