9b142da7e99d2fe6120da021f315e90dd1802ce7
atomi <1456417373@qq.com>
2023-02-21 19:05:52 +0800

    perf: Update the menu permission policy.

--- fastapi_amis_admin/admin/admin.py
+++ fastapi_amis_admin/admin/admin.py
@@ -1177,17 +1177,17 @@ class AdminGroup(PageSchemaAdmin):
     async def get_page_schema_children(self, request: Request) -> List[PageSchema]:
         page_schema_list = []
         for child in self._children:
-            if not child.page_schema or not await child.has_page_permission(request, action="page"):
+            if not child.page_schema:
                 continue
             if (isinstance(child, AdminGroup) and not isinstance(child, AdminApp)) or (
                 isinstance(child, AdminApp) and child.page_schema.tabsMode is None
             ):
                 sub_children = await child.get_page_schema_children(request)
-                if sub_children:
+                if sub_children:  # If there are sub-nodes, show them even if the parent node has no permission.
                     page_schema = child.page_schema.copy(deep=True)
                     page_schema.children = sub_children
                     page_schema_list.append(page_schema)
-            else:
+            elif await child.has_page_permission(request, action="page"):
                 page_schema_list.append(child.page_schema)
         if page_schema_list:
             page_schema_list.sort(key=lambda p: p.sort or 0, reverse=True)
