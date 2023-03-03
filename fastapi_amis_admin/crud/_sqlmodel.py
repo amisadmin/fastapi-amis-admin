00f90e6857b3141c96523bdc7840ea1adc0b0822
atomi <1456417373@qq.com>
2023-03-03 09:14:52 +0800

    fix: Avoid modifying class variables.

--- fastapi_amis_admin/crud/_sqlmodel.py
+++ fastapi_amis_admin/crud/_sqlmodel.py
@@ -101,15 +101,15 @@ class SQLModelSelector(Generic[SchemaModelT]):
         self.pk_name: str = self.pk_name or self.model.__table__.primary_key.columns.keys()[0]
         self.pk: InstrumentedAttribute = self.model.__dict__[self.pk_name]
         self.parser = SQLModelFieldParser(self.model)
-        self.fields = fields or self.fields or [self.model]
+        fields = fields or self.fields or [self.model]
         exclude = self.parser.filter_insfield(self.exclude)
         self.fields = [
             sqlfield
-            for sqlfield in self.parser.filter_insfield(self.fields + [self.pk], save_class=(Label,))
+            for sqlfield in self.parser.filter_insfield(fields + [self.pk], save_class=(Label,))
             if sqlfield not in exclude
         ]
         assert self.fields, "fields is None"
-        self.list_filter = self.list_filter or self.fields
+        self.list_filter = self.list_filter and self.list_filter.copy() or self.fields
         self.link_models = self.link_models or {}
         """Make sure the value of link_models is an object attribute, not a class attribute."""
 
