e8af6cbafc3a31a01a924bf57b75084056adb248
atomi <1456417373@qq.com>
2023-04-28 21:46:12 +0800

    perf: Optimize `as_page_body` logic in `PageSchema`.

--- fastapi_amis_admin/amis/components.py
+++ fastapi_amis_admin/amis/components.py
@@ -396,35 +396,46 @@ class PageSchema(AmisNode):
 
     def as_page_body(self, group_extra: Dict[str, Any] = None, item_extra: Dict[str, Any] = None):
         if self.children:
+            exclude = {"type", "url", "schema_", "schemaApi", "link", "redirect", "rewrite", "isDefaultPage", "children"}
             if self.tabsMode is None:
                 body = App(pages=[PageSchema(children=self.children)])
             elif self.tabsMode == TabsModeEnum.collapse:
-                body = CollapseGroup(
-                    body=[
-                        CollapseGroup.CollapseItem(
-                            header=item.label,
-                            body=item.as_page_body(group_extra, item_extra),
-                        ).update_from_dict(item_extra or {})
-                        for item in self.children
-                    ],
-                ).update_from_dict(group_extra or {})
+                body = (
+                    CollapseGroup.parse_obj(self.dict(exclude=exclude, exclude_defaults=True))
+                    .update_from_kwargs(
+                        body=[
+                            CollapseGroup.CollapseItem.parse_obj(item.dict(exclude=exclude, exclude_defaults=True))
+                            .update_from_kwargs(
+                                header=item.label,
+                                body=item.as_page_body(group_extra, item_extra),
+                            )
+                            .update_from_dict(item_extra or {})
+                            for item in self.children
+                        ],
+                    )
+                    .update_from_dict(group_extra or {})
+                )
             else:
-                body = Tabs(
-                    tabsMode=self.tabsMode,
-                    mountOnEnter=True,
-                    tabs=[
-                        Tabs.Item(
-                            title=item.label,
-                            icon=item.icon,
-                            tab=item.as_page_body(group_extra, item_extra),
-                        ).update_from_dict(item_extra or {})
-                        for item in self.children
-                    ],
-                ).update_from_dict(group_extra or {})
+                body = (
+                    Tabs.parse_obj(self.dict(exclude=exclude, exclude_defaults=True))
+                    .update_from_kwargs(
+                        mountOnEnter=True,
+                        tabs=[
+                            Tabs.Item.parse_obj(item.dict(exclude=exclude, exclude_defaults=True))
+                            .update_from_kwargs(
+                                title=item.label,
+                                tab=item.as_page_body(group_extra, item_extra),
+                            )
+                            .update_from_dict(item_extra or {})
+                            for item in self.children
+                        ],
+                    )
+                    .update_from_dict(group_extra or {})
+                )
         elif self.schema_:
             body = self.schema_
             if isinstance(body, Iframe):
-                body.height = 1080
+                body.height = body.height or 1080
         elif self.schemaApi:
             body = Service(schemaApi=self.schemaApi)
         elif self.link:
