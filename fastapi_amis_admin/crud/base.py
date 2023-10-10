7d3e2cb3bd33a1c72e5afd029f8e0a7ea21c7dca
atomi <1456417373@qq.com>
2023-10-10 21:44:23 +0800

    perf: HTTPException is not handled when executing SQL errors

--- fastapi_amis_admin/crud/base.py
+++ fastapi_amis_admin/crud/base.py
@@ -216,6 +216,8 @@ class BaseCrud(RouterMixin, Generic[SchemaModelT, SchemaListT, SchemaFilterT, Sc
                 status_code=status.HTTP_422_UNPROCESSABLE_ENTITY,
                 detail="Key already exists",
             ) from error
+        elif isinstance(error, HTTPException):
+            raise error
         raise HTTPException(
             status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
             detail=f"Error Execute SQL：{error}",
