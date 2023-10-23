38d7cc0ecc7a310c8119fc0563108507618f9d3a
atomi <1456417373@qq.com>
2023-10-23 10:48:03 +0800

    fix: Fixed whitespace character parsing

--- fastapi_amis_admin/crud/utils.py
+++ fastapi_amis_admin/crud/utils.py
@@ -23,7 +23,7 @@ IdStrQuery = Annotated[
 def parser_str_set_list(item_id: Union[int, str]) -> List[str]:
     if isinstance(item_id, int):
         return [str(item_id)]
-    elif not isinstance(item_id, str):
+    elif not isinstance(item_id, str) or not item_id:
         return []
     return list(set(item_id.split(",")))
 
