2a31fb02c840cd2d20ceeb92b72c7b8caa99115f
atomi <1456417373@qq.com>
2023-12-18 15:47:57 +0800

    perf: The Field method adds support for pydantic v2 parameters

--- fastapi_amis_admin/models/fields.py
+++ fastapi_amis_admin/models/fields.py
@@ -1,4 +1,4 @@
-from typing import AbstractSet, Any, Callable, Dict, Mapping, Optional, Sequence, Type, Union
+from typing import AbstractSet, Any, Callable, Dict, List, Literal, Mapping, Optional, Sequence, Type, Union
 
 from sqlalchemy import Column
 from sqlmodel._compat import Undefined, UndefinedType, post_init_field_info
@@ -7,14 +7,25 @@ from sqlmodel.main import FieldInfo, NoArgAnyCallable
 from fastapi_amis_admin.amis import FormItem, TableColumn
 from fastapi_amis_admin.utils.pydantic import PYDANTIC_V2
 
+if PYDANTIC_V2:
+    from pydantic.fields import AliasChoices, AliasPath
+
+    ValidationAliasT = Union[str, AliasPath, AliasChoices, None]
+else:
+    ValidationAliasT = Optional[str]
+
 
 def Field(
     default: Any = Undefined,
     *,
     default_factory: Optional[NoArgAnyCallable] = None,
     alias: Optional[str] = None,
+    alias_priority: Optional[int] = Undefined,
+    validation_alias: ValidationAliasT = Undefined,
+    serialization_alias: Optional[str] = Undefined,
     title: Optional[str] = None,
     description: Optional[str] = None,
+    examples: Optional[List[Any]] = Undefined,
     exclude: Union[AbstractSet[Union[int, str]], Mapping[Union[int, str], Any], Any] = None,
     include: Union[AbstractSet[Union[int, str]], Mapping[Union[int, str], Any], Any] = None,
     const: Optional[bool] = None,
@@ -34,6 +45,14 @@ def Field(
     regex: Optional[str] = None,
     discriminator: Optional[str] = None,
     repr: bool = True,
+    init_var: Optional[bool] = Undefined,
+    kw_only: Optional[bool] = Undefined,
+    pattern: Optional[str] = Undefined,
+    strict: Optional[bool] = Undefined,
+    allow_inf_nan: Optional[bool] = Undefined,
+    union_mode: Literal["smart", "left_to_right"] = Undefined,
+    frozen: Optional[bool] = Undefined,
+    validate_default: Optional[bool] = Undefined,
     primary_key: Union[bool, UndefinedType] = Undefined,
     foreign_key: Any = Undefined,
     unique: Union[bool, UndefinedType] = Undefined,
@@ -58,6 +77,18 @@ def Field(
     if PYDANTIC_V2:
         current_schema_extra = {
             "json_schema_extra": current_schema_extra,
+            "alias_priority": alias_priority,
+            "validation_alias": validation_alias,
+            "serialization_alias": serialization_alias,
+            "examples": examples,
+            "init_var": init_var,
+            "kw_only": kw_only,
+            "pattern": pattern,
+            "strict": strict,
+            "allow_inf_nan": allow_inf_nan,
+            "union_mode": union_mode,
+            "frozen": frozen,
+            "validate_default": validate_default,
         }
     field_info = FieldInfo(
         default,
