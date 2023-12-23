476bc7933d938a3b4bae2e3dedaae7d15b4c810f
atomi <1456417373@qq.com>
2023-12-23 16:38:53 +0800

    perf: Optimized `ChoiceType`

--- fastapi_amis_admin/models/_sqltypes.py
+++ fastapi_amis_admin/models/_sqltypes.py
@@ -1,28 +1,17 @@
 from typing import Type
 
 from sqlalchemy import types
-from sqlalchemy.engine import Dialect
 
 from ._enums import Choices
 
 
-class ChoiceType(types.TypeDecorator):
-    impl = types.CHAR
-    cache_ok = True
-
+class BaseChoicesType(types.TypeDecorator):
     def __init__(self, choices: Type[Choices], impl=None, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.choices = choices
 
-    @property
-    def python_type(self):
-        return self.impl.python_type
-
-    def load_dialect_impl(self, dialect: Dialect):
-        return dialect.type_descriptor(types.CHAR(20))
-
     def process_bind_param(self, value, dialect):
-        if value and isinstance(value, Choices):
+        if value is not None and isinstance(value, Choices):
             return value.value
         return value
 
@@ -32,3 +21,17 @@ class ChoiceType(types.TypeDecorator):
                 value = int(value)
             return self.choices(value)
         return value
+
+
+class TextChoicesType(BaseChoicesType):
+    impl = types.CHAR
+    cache_ok = True
+
+
+class IntegerChoicesType(BaseChoicesType):
+    impl = types.Integer
+    cache_ok = True
+
+
+def ChoiceType(choices: Type[Choices], *args, **kwargs):
+    return (IntegerChoicesType if issubclass(choices, int) else TextChoicesType)(choices, *args, **kwargs)
